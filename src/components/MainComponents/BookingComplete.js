db4edd823093a18fd8b31a5d850839438c279c8d
khatibtamal <khatibtamal@gmail.com>
2024-11-17 21:54:02 +0600

    completed booking complete section

--- src/components/MainComponents/BookingComplete.js
+++ src/components/MainComponents/BookingComplete.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import "./BookingComplete.css";
 
 function BookingComplete() {
     const navigate = useNavigate();
@@ -8,12 +9,13 @@ function BookingComplete() {
     }
 
     return (
-        <div>
-            <h1>Booking Complete!</h1>
-            {/* <a href="/"> */}
-                <button className="app-button" onClick={ handleClick }>Home</button>
-            {/* </a> */}
-        </div>
+        <section class="booking-complete-section">
+            <h1>Reservation Complete!</h1>
+            <p>John Doe, your reservation is complete for 45th of Luna 3421!</p>
+            <p>You will soon receive and email with reservation details.</p>
+            <p>If you choose to edit or cancel the reservation, please follow the <span>Manage Booking</span> section in the email.</p>
+            <button className="app-button" onClick={ handleClick }>Home</button>
+        </section>
     );
 }
 
